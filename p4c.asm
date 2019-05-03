65aea3c18fd7fec84353115d0a3158b9bbc0f166
solanav <solanav@qq.com>
2019-05-03 08:17:03 +0100

    Added compares

--- p4c.asm
+++ p4c.asm
@@ -5,9 +5,11 @@
 ;**************************************************************************
 ; definicion del segmento de datos
 datos segment
-INPUT_ASK	DB	"Introduce una cadena de caracteres:", 10, 13
-ERROR	DB	"You did something wrong", 10, 13
-INPUT	DB	128 DUP(0)
+INPUT_ASK	db "Introduce una cadena de caracteres:", 10, 13
+ERROR		db "You did something wrong", 10, 13
+INPUT		db 128 DUP(0)
+ENCODE		db "encode"
+DECODE		db "decode"
 datos ends
 
 pila segment stack "stack"
@@ -38,18 +40,66 @@ inicio proc
 	call comp_encode
 	je encode
 
+user_error:
 	mov ah, 9h
 	mov dx, OFFSET ERROR
 	int 21h
 
+end_end:
 	mov ah, 4Ch
 	int 21h 
 
-	get_input:
-		mov dx, OFFSET INPUT
-		mov ah, 0Ah
-		int 21h
-		xor bx, bx
+get_input:
+	mov dx, OFFSET INPUT
+	mov ah, 0Ah
+	int 21h
+	xor bx, bx
+
+cmp_decode:
+; Takes data from INPUT
+; returns ax = 1 if it is equal to "decode"
+	xor si, si
+
+	cmp_d_loop:
+	mov ax, INPUT[si]
+	mov bx, DECODE[si]
+	cmp ax, bx
+	jne end_d_nequal
+
+	inc si
+	cmp si, 7 ; 6 de "decode" + 1 del "$"
+	jne cmp_d_loop
+
+	end_d_equal:
+	mov ax, 1
+	ret
+
+	end_d_nequal:
+	mov ax, 0
+	ret
+
+cmp_encode:
+; Takes data from INPUT
+; returns ax = 1 if it is equal to "encode"
+	xor si, si
+
+	cmp_e_loop:
+	mov ax, INPUT[si]
+	mov bx, ENCODE[si]
+	cmp ax, bx
+	jne end_e_nequal
+
+	inc si
+	cmp si, 7 ; 6 de "encode" + 1 del "$"
+	jne cmp_e_loop
+
+	end_e_equal:
+	mov ax, 1
+	ret
+
+	end_e_nequal:
+	mov ax, 0
+	ret
 
 inicio endp
 
